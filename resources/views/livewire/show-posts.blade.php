5e690df920e55a8c1383863e7d70a7a25a513b28
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-07-13 15:39:03 -0400

    Recuperamos los datos y Agregamos el Buscador

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -4,6 +4,55 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <h1>Hola Mundo</h1>
-    {{$posts}}
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <x-table>
+            <div class="px-6 py-4">
+                <x-jet-input class="w-full" placeholder="Buscar..." type="text" wire:model="search"></x-jet-input>
+            </div>
+            @if ($posts->count())
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Id
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Title
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Content
+                        </th>
+                        <th scope="col" class="relative px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach ($posts as $post)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$post->id}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$post->title}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                <div class="text-sm text-gray-900">{{$post->content}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    <!-- More people... -->
+                    </tbody>
+                </table>
+            @else
+                <div class="px-6 py-4">
+                    No se encontro ningun registro
+                </div>
+            @endif
+        </x-table>
+    </div>
 </div>
