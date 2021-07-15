a3883732db5c29a9b92363599eada3155c3d4330
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-07-14 21:37:43 -0400

    Ordenar las los elemento de las tabla de manera ascednente

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -8,10 +8,13 @@ use App\Models\Post;
 class ShowPosts extends Component
 {
     public $search;
+    public $sort = 'id';
+    public $direction = 'desc'
     public function render()
     {
         $posts = Post::where('title','like','%'.$this->search.'%')
-                        ->orWhere('content','like','%'.$this->search.'%')->get();
+                        ->orWhere('content','like','%'.$this->search.'%')
+                        ->orderBy($this->sort,$this->direction)->get();
         return view('livewire.show-posts',compact('posts'));
     }
 }
