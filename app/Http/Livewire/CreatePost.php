bffd1ddb16c211fab062716245cad4cf3de3695a
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-07-18 11:44:30 -0400

    Guardamos Imagenes al crear posts

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -4,25 +4,34 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
-
+use Livewire\WithFileUploads;
 class CreatePost extends Component
 {
+    use WithFileUploads;
     public $open = false;
-    public $title, $content;
+    public $title, $content,$image, $identificador;
+
+    public function mount(){
+        $this->identificador = rand();
+    }
 
     protected $rules = [
-        'title' => 'required|max:10',
-        'content' => 'required|max:100',
+        'title' => 'required',
+        'content' => 'required',
+        'image' => 'required',
     ];
 
     public function save(){
         $this->validate();
+        $image = $this->image->store('posts');
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
+            'image' => $image,
         ]);
         $this->reset(['open', 'title', 'content']);
         $this->emitTo('show-posts','render');
+        $this->identificador = rand();
         $this->emit('alert','Post Creado con Exito');
     }
     public function render()
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -6,6 +6,13 @@
             Crear Nuevo Post
         </x-slot>
         <x-slot name="content">
+            <div wire:loading wire:target="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">!Cargando Imagen!</strong>
+                <span class="block sm:inline">Espere mientras carga la Imagen.</span>
+              </div>
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}" alt="Imagen Seleccionada">
+            @endif
             <div class="mb-4">
                 <x-jet-label>Titulo del Post</x-jet-label>
                 <x-jet-input class="w-full" type="text" wire:model="title" />
@@ -16,10 +23,14 @@
                 <textarea class="form-control w-full" type="text" rows="6" wire:model.defer="content"></textarea>
                 <x-jet-input-error for="content"/>
             </div>
+            <div>
+                <input type="file" wire:model="image" id="{{$identificador}}">
+                <x-jet-input-error for="image"/>
+            </div>
         </x-slot>
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('open',false)">Cancelar</x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save" class="disabled:opacity-25">Crear</x-jet-danger-button>
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">Crear</x-jet-danger-button>
         </x-slot>
     </x-jet-dialog-modal>
 </div>
